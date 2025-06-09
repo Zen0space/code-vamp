1cb2dd274de3d771e71278cd4b1cd19dc75b4dc1
Khairul Anuar <tartnenas_kai@outlook.com>
2025-06-09 08:50:24 +0800

    next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,9 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
+  // Required secret for production
+  secret: process.env.NEXTAUTH_SECRET,
+  
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
     {
