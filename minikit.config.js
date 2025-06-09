86d500995a581081785223da968fc47cb3546fdf
Khairul Anuar <tartnenas_kai@outlook.com>
2025-06-09 10:29:15 +0800

    test

--- minikit.config.js
+++ minikit.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@worldcoin/minikit-js').MiniKitConfig} */
 module.exports = {
-  app_id: process.env.NEXT_PUBLIC_MINIKIT_APP_ID || 'your-app-id',
+  app_id: process.env.NEXT_PUBLIC_MINIKIT_APP_ID || 'app_812ac0842eb746d6a8bbe07db8b7cc1e',
   name: 'CodeVamp',
   description: 'Master programming with the hunger of a vampire',
   logo_url: 'https://codevamp.zen0.space/logo.png', // Add your logo URL
