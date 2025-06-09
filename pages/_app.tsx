e87813ee187ac2691ec0b032ae2082b27626a78a
Khairul Anuar <tartnenas_kai@outlook.com>
2025-06-09 11:05:17 +0800

    hm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { VerificationProvider } from '../utils/VerificationContext'
 
 // Initialize MiniKit for World App integration
 if (typeof window !== 'undefined') {
-  MiniKit.install()
+  MiniKit.install(process.env.NEXT_PUBLIC_MINIKIT_APP_ID || 'app_812ac0842eb746d6a8bbe07db8b7cc1e')
 }
 
 export default function App({ Component, pageProps }: AppProps) {
